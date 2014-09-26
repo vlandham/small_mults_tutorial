2508be18df9f7b7d78b318edfa053947d18466b9
Jim V <vlandham@gmail.com>
2014-09-25 20:28:08 -0700

    update r script

--- linked_small_multiples/tools/aggregate_metafilter.R
+++ linked_small_multiples/tools/aggregate_metafilter.R
@@ -6,18 +6,17 @@ require("stringr")
 #http://mefiwiki.com/wiki/Infodump
 #http://stuff.metafilter.com/infodump/
 
-askmefi <- read.table("~/Downloads/postdata_askme.txt", header = TRUE, sep = "\t", skip = 1, fill = TRUE)
-cats <- read.table("~/Desktop/small_mults_tutorial/code/data/askmefi_cats.txt", header = TRUE, sep = "\t")
+# needs these two files to function. one is the data dump of new posts. 
+# The other is the category mapppings from the wiki.
+# I've included the category mappings because its tiny, but not the 
+# posts dump - thats too big for me to include here.
+
+askmefi <- read.table("./postdata_askme.txt", header = TRUE, sep = "\t", skip = 1, fill = TRUE)
+cats <- read.table("./askmefi_cats.txt", header = TRUE, sep = "\t")
 
 askmefi <- merge(askmefi, cats, by.x = c("category"), by.y = c("CatID"))
 
-#askmefi$date <-  mdy_hms(askmefi$datestamp)
-
-#splits <- strsplit(as.character(askmefi$datestamp), split= " ")
-
-#mon_year <- apply(data.frame(askmefi$datestamp), 1, function(r) {x <- strsplit(as.character(r), split= " "); return(paste(x[1], x[2], sep="-"))})
-
-# some of the datestamps had a doulbe space in them...
+# some of the datestamps has a double space in them...
 clean_stamps <- gsub("  ", " ", askmefi$datestamp)
 dates <- str_split_fixed(clean_stamps, " ", 4)
 dates_df <- data.frame(dates)
@@ -29,17 +28,13 @@ askmefi_dates <- cbind(askmefi, dates_df)
 askmefi_months <- summarise(group_by(askmefi_dates, mon_year, Description), n = n())
 
 colnames(askmefi_months) <- c('mon_year', 'category', 'n')
-write.table(askmefi_months, file = "~/Desktop/askmefi_category_month.tsv", row.names = FALSE, col.names = TRUE, sep = "\t")
+# write out the month level data - in case you want to see that
+write.table(askmefi_months, file = "./askmefi_category_month.tsv", row.names = FALSE, col.names = TRUE, sep = "\t")
 
 askmefi_years <- summarise(group_by(askmefi_dates, year, Description), n = n())
 askmefi_years <- filter(askmefi_years, year != "2003")
 colnames(askmefi_years) <- c('year', 'category', 'n')
-write.table(askmefi_years, file = "~/Desktop/askmefi_category_year.tsv", row.names = FALSE, col.names = TRUE, sep = "\t")
 
-p <- ggplot(askmefi, aes(x = category)) + geom_histogram(binwidth = 1)
-p
+# write out year data
+write.table(askmefi_years, file = "./askmefi_category_year.tsv", row.names = FALSE, col.names = TRUE, sep = "\t")
 
-
-by_cat <- group_by(askmefi, category)
-summ <- summarise(by_cat, n = n())
-summ
